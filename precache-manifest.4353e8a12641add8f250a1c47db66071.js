4f1d796ee6de67e1998b40269c7909a30f0c924d
Nutan Sandesh Salunkhe <nutan.salunkhe@yash.com>
2019-12-03 13:22:36 +0530

    Updates

---
+++ precache-manifest.4353e8a12641add8f250a1c47db66071.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "327fa2bd78336aa3a1f69332f78e6e4d",
+    "url": "/NutanBhosale/pta-react.git/index.html"
+  },
+  {
+    "revision": "5f07cc3cb5824595359b",
+    "url": "/NutanBhosale/pta-react.git/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "972cfe316129f478a358",
+    "url": "/NutanBhosale/pta-react.git/static/js/2.f7ca0266.chunk.js"
+  },
+  {
+    "revision": "5f07cc3cb5824595359b",
+    "url": "/NutanBhosale/pta-react.git/static/js/main.e7caef19.chunk.js"
+  },
+  {
+    "revision": "f37422ac5f3afe0c8d9e",
+    "url": "/NutanBhosale/pta-react.git/static/js/runtime-main.cf6ca74a.js"
+  }
+]);
